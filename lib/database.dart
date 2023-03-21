d8664937ebdefaedca90c1f60fcea5451a2d7d97
Stuart Bowes <stuartbowes2000@gmail.com>
2023-03-21 11:25:09 +0000

    Add database access

---
+++ lib/database.dart
@@ -0,0 +1,44 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
+import 'film_model.dart';
+
+class DataBase {
+  Future<Database> initializeDB() async {
+    String path = await getDatabasesPath();
+    return openDatabase(
+      join(path, 'films.db'),
+      version: 1,
+      onCreate: (Database db, int version) async {
+        await db.execute(
+          "CREATE TABLE films(id INTEGER PRIMARY KEY, title TEXT NOT NULL, director TEXT NOT NULL, releaseYear INTEGER NOT NULL",
+        );
+      },
+    );
+  }
+
+  Future<int> addFilms(List<Film> films) async {
+    int result = 0;
+    final Database db = await initializeDB();
+    for (var film in films) {
+      result = await db.insert('films', film.toMap(),
+          conflictAlgorithm: ConflictAlgorithm.replace);
+    }
+    return result;
+  }
+
+  Future<List<Film>> getAllFilms() async {
+    final Database db = await initializeDB();
+    final List<Map<String, Object?>> queryResult = await db.query('films');
+    return queryResult.map((e) => Film.fromMap(e)).toList();
+  }
+
+  Future<void> deleteFilm(int id) async {
+    final db = await initializeDB();
+    await db.delete(
+      'films',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
